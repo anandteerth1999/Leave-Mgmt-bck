e37d4835cb7e8c8d99b19cd1ad7e31be89379267
abhishekbn87 <abhishekbn87@gmail.com>
2020-12-12 12:19:18 +0530

    Refined non teaching staff interface

--- leave.py
+++ leave.py
@@ -9,7 +9,7 @@ from json import dumps, dump
 from mail import *
 from documents import *
 from datetime import date, timedelta, datetime
-from holidays import getHolidays
+
 
 config = {
     'apiKey': "AIzaSyBXPWOPBLz87VXW5Hh_DGVSxw8Ak23pgEM",
@@ -21,6 +21,8 @@ config = {
     'appId': "1:116832669800:web:19e5f5de80209738ffebdd",
     'measurementId': "G-B2YEJY3CH1"
 }
+
+
 
 firebase = pyrebase.initialize_app(config)
 s = firebase.storage()
@@ -34,7 +36,9 @@ api = Api(app)
 result = []
 dict = {}
 CORS(app)
-holidays = getHolidays(str(datetime.now().year))
+holidays = ['2020-01-05', '2020-01-12', '2020-01-15', '2020-01-19', '2020-01-26', '2020-02-02', '2020-02-09', '2020-02-16', '2020-02-21', '2020-02-23', '2020-03-01', '2020-03-08', '2020-03-15', '2020-03-22', '2020-03-25', '2020-03-29', '2020-04-01', '2020-04-05', '2020-04-06', '2020-04-10', '2020-04-12', '2020-04-14', '2020-04-19', '2020-04-26', '2020-05-01', '2020-05-03', '2020-05-10', '2020-05-17', '2020-05-24', '2020-05-25', '2020-05-31', '2020-06-07', '2020-06-14', '2020-06-21', '2020-06-28', '2020-07-05',
+            '2020-07-12', '2020-07-19', '2020-07-26', '2020-08-01', '2020-08-02', '2020-08-09', '2020-08-15', '2020-08-16', '2020-08-22', '2020-08-23', '2020-08-30', '2020-09-06', '2020-09-13', '2020-09-17', '2020-09-20', '2020-09-27', '2020-10-02', '2020-10-04', '2020-10-11', '2020-10-18', '2020-10-25', '2020-10-26', '2020-10-30', '2020-10-31', '2020-11-01', '2020-11-08', '2020-11-14', '2020-11-15', '2020-11-16', '2020-11-22', '2020-11-29', '2020-12-03', '2020-12-06', '2020-12-13', '2020-12-20', '2020-12-25', '2020-12-27']
+
 
 
 class Faculty_details(Resource):
@@ -214,20 +218,38 @@ class Apply_Leave(Resource):
 class Lecturer_details(Resource):
     def get(self, email):
         result.clear()
+        flag = Check_Teaching().get(email)
+        print(flag)
         conn = e.connect()
-        query = conn.execute(
-            'select Name,Fid,Designation,email,Phno from Teaching where Teaching.email !=' + '\'' + email + '\'')
-        for i in query.cursor.fetchall():
-            dict = {
-                'name': i[0],
-                'fid': i[1],
-                'designation': i[2],
-                'url': s.child(i[1] + '.jpg').get_url(None),
-                'email': i[3],
-                'phone': i[4]
+        if flag == True:
+            query = conn.execute(
+                'select Name,Fid,Designation,email,Phno from Teaching where Teaching.email !=' + '\'' + email + '\'')
+            for i in query.cursor.fetchall():
+                dict = {
+                    'name': i[0],
+                    'fid': i[1],
+                    'designation': i[2],
+                    'url': s.child(i[1] + '.jpg').get_url(None),
+                    'email': i[3],
+                    'phone': i[4]
 
-            }
-            result.append(dict)
+                }
+                result.append(dict)
+        else:
+            query = conn.execute(
+                'select Name,Fid,Designation,email,Phno from Non_Teaching where Non_Teaching.email !=' + '\'' + email + '\'')
+            for i in query.cursor.fetchall():
+                dict = {
+                    'name': i[0],
+                    'fid': i[1],
+                    'designation': i[2],
+                    'url': s.child(i[1] + '.jpg').get_url(None),
+                    'email': i[3],
+                    'phone': i[4]
+
+                }
+                result.append(dict)
+
         return result
 
 
@@ -295,41 +317,70 @@ class Remaining_leaves(Resource):
 
 class From_Alt(Resource):
     def get(self, email):
+        flag = Check_Teaching().get(email)
         result.clear()
         conn = e.connect()
-        from_alt = conn.execute(
-            'select Applied_date , sem,sec,subject,Applied_time , T_email , T.Name from alternate , Teaching T where T.Fid = alternate.T_fid and F_email = ' + '\'' + email + '\'').fetchall()
-        for alt in from_alt:
-            dict = {
-                'date': alt[0],
-                'sem': alt[1],
-                'sec': alt[2],
-                'subject': alt[3],
-                'time': alt[4],
-                'email': alt[5] ,
-                'name': alt[6]
-            }
-            result.append(dict)
+        if flag :
+            from_alt = conn.execute(
+                'select Applied_date , sem,sec,subject,Applied_time , T_email , T.Name from alternate , Teaching T where T.Fid = alternate.T_fid and F_email = ' + '\'' + email + '\'').fetchall()
+            for alt in from_alt:
+                dict = {
+                    'date': alt[0],
+                    'sem': alt[1],
+                    'sec': alt[2],
+                    'subject': alt[3],
+                    'time': alt[4],
+                    'email': alt[5] ,
+                    'name': alt[6]
+                }
+                result.append(dict)
+        else:
+            from_alt = conn.execute(
+                'select Applied_date , Applied_time , T_email , T.Name from Non_Teaching_alternate , Non_Teaching T where T.Fid = Non_Teaching_alternate.T_fid and F_email = ' + '\'' + email + '\'').fetchall()
+            for alt in from_alt:
+                dict = {
+                    'date': alt[0],
+                    'time': alt[1],
+                    'email': alt[2] ,
+                    'name': alt[3]
+                }
+                result.append(dict)
+
         return result
 
 
 class To_Alt(Resource):
     def get(self, email):
+        flag = Check_Teaching().get(email)
         result.clear()
         conn = e.connect()
-        from_alt = conn.execute(
-            'select Applied_date , sem,sec,subject,Applied_time , F_email  , T.name from alternate , Teaching T where T.Fid = alternate.F_fid and T_email = ' + '\'' + email + '\'').fetchall()
-        for alt in from_alt:
-            dict = {
-                'date': alt[0],
-                'sem': alt[1],
-                'sec': alt[2],
-                'subject': alt[3],
-                'time': alt[4],
-                'email': alt[5] ,
-                'name': alt[6]
-            }
-            result.append(dict)
+        if flag:
+            from_alt = conn.execute(
+                'select Applied_date , sem,sec,subject,Applied_time , F_email  , T.name from alternate , Teaching T where T.Fid = alternate.F_fid and T_email = ' + '\'' + email + '\'').fetchall()
+            for alt in from_alt:
+                dict = {
+                    'date': alt[0],
+                    'sem': alt[1],
+                    'sec': alt[2],
+                    'subject': alt[3],
+                    'time': alt[4],
+                    'email': alt[5] ,
+                    'name': alt[6]
+                }
+                result.append(dict)
+        else:
+            from_alt = conn.execute(
+                'select Applied_date , Applied_time , F_email , T.Name from Non_Teaching_alternate , Non_Teaching T where T.Fid = Non_Teaching_alternate.F_fid and T_email = ' + '\'' + email + '\'').fetchall()
+            for alt in from_alt:
+                dict = {
+                    'date': alt[0],
+                    'time': alt[1],
+                    'email': alt[2],
+                    'name': alt[3]
+                }
+                result.append(dict)
+
+
         return result
 
 
@@ -483,6 +534,23 @@ class Upload_File(Resource):
         print('Upload successful')
         print(file)
         return "Done"
+
+class Non_Teaching_Alternate(Resource):
+    def post(self,email,date,time,faculty):
+        conn = e.connect()
+        from_fid = conn.execute(
+            'select Fid from Non_Teaching where Non_Teaching.email =' + '\'' + email + '\'').fetchall()[0][0]
+        from_name = conn.execute(
+            'select name from Non_Teaching where Non_Teaching.email =' + '\'' + email + '\'').fetchall()[0][0]
+        to_email = conn.execute(
+            'select email from Non_Teaching where Non_Teaching.Name = ' + '\'' + faculty + '\'').fetchall()[0][0]
+        to_fid = conn.execute(
+            'select fid from Non_Teaching where Non_Teaching.Name = ' + '\'' + faculty + '\'').fetchall()[0][0]
+        # alternate(from_name, date, sem, sub, time, to_email, sec)
+        values = "('%s' , '%s' , '%s' ,'%s', '%s' , '%s')" % (
+            email, date,  time, from_fid, to_email, to_fid)
+        query = conn.execute('insert into Non_Teaching_alternate values ' + values)
+
  
 
 api.add_resource(Register_User , '/api/register')
@@ -509,6 +577,7 @@ api.add_resource(Check_HOD, '/api/checkHOD/<string:email>')
 api.add_resource(SignUp_User , '/api/signUp/<string:email>/<string:password>')
 api.add_resource(Check_User , '/api/getFIDs')
 api.add_resource(Upload_File , '/api/upload/<string:fid>')
+api.add_resource(Non_Teaching_Alternate , '/api/nonTeachingAlternate/<string:email>/<string:date>/<string:time>/<string:faculty>')
 
 if __name__ == '__main__':
     app.run(debug=True)
